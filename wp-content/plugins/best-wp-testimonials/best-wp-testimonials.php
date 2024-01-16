30b93cbb52c004a54ab9793606b69dda5f0bdda5
Md Jahirul Islam <mdjoherul50@gmail.com>
2024-01-16 23:35:47 +0600

    updated

--- wp-content/plugins/best-wp-testimonials/best-wp-testimonials.php
+++ wp-content/plugins/best-wp-testimonials/best-wp-testimonials.php
@@ -16,9 +16,10 @@
  */
 
 /**
- * Proper way to enqueue scripts and styles
+ * bwpt styles css
  */
 function bwpt_enqueue_style() {
+   //wp_enqueue_style('string $handle', mixed $src, array $deps, mixed $ver, string $meida );
 	wp_enqueue_style( 'owl.carousel', 'https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css' );
     wp_enqueue_style( 'owl.theme', 'https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css' );
     wp_enqueue_style( 'bwpt-style', plugins_url( 'css/bwpt-style.css', __FILE__ ) );
@@ -26,6 +27,14 @@ function bwpt_enqueue_style() {
 }
 add_action( 'wp_enqueue_scripts', 'bwpt_enqueue_style');
 
+/* bwpt js style add*/
+function bwpt_enqueue_script(){
+    //wp_enqueue_style('string $handle', mixed $src, array $deps, mixed $ver, bol $in_footer );
+    wp_enqueue_script('adsensejs', 'https://pagead2.googlesyndication.com/pagead/managed/js/adsense/m202401110101/show_ads_impl_fy2021.js?bust=31080431', array(), '1.0.0', 'true' );
+    wp_enqueue_script('owl.carousel', 'https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js', array(), '1.0.0', 'true' );
+    wp_enqueue_script('bwpt_script', plugins_url( 'js/bwpt_script.js', __FILE__ ), array(), '1.0.0', 'true' );
+}
+add_action('wp_enqueue_scripts', 'bwpt_enqueue_script');
 
 
 
